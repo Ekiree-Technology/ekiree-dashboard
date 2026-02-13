ea0f63ebffae048ed0d26cd77294bc4deeb376ab
Noah Wilson <51177491+Kaddu720@users.noreply.github.com>
2026-02-12 17:08:23 -0800

    40-clean-up-ck-editor-5 (#119)

--- ekiree_dashboard/ed/urls.py
+++ ekiree_dashboard/ed/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -56,5 +56,4 @@ urlpatterns = [
         views.ApproveAppCourseReplacement,
         name='approveReplace'
     ),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
