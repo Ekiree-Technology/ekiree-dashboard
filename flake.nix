7b999764d23c36ae1dc138a9daaecac2ff8b763f
Kaddu720 <nwilsonmalgus@gmail.com>
2026-02-12 16:10:55 -0800

    revert djanog stubs

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     poetryDev = mkPoetryEnv {
       projectDir = ./.;
       preferWheels = true;
-      extraPackages = ps: [ps.pip];
+      extraPackages = ps: [ps.pip ps.django-stubs];
       overrides = overrides.withDefaults (final: prev: {
         reportlab = prev.reportlab.override {
           preferWheel = false;
